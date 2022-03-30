994383881f8946da4dbae2197375bbe5b8486f27
Sarah W <83520578+andromedamoon-stack@users.noreply.github.com>
2022-03-30 20:50:39 +0000

    refined into python code, added names to turples.

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ print("Welcome to the Psych Sidekick Name Generator")
 print("A name that Shawn would give Gus:'\n'")
 
 
-first = ()
-last = ()
+first = ('SuperSmeller','Francois','Peter Panic','Felicia','Gus','Ovaltine','Shmuel','Magic','Don','Galileo','The','Schoonie','Lavender','Hummingbird','Scrooge','Sterling','Dr.','Step','Trapezious','Jazz','Doughnut','Clemintine','The Vault Of','Ghee','Pennywhistle', 'Pitchfork Ben', 'Potato Bug','Pushmeet','Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+"Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+"Winston 'Jazz Hands'", 'Worms')
+last = ('Fancybottom', 'Smeller','Lambert Watkins','T.T Showbiz','Jenkins','Cohen','Head','Humpkins','Jackal','U-turn SIngleton','Gooms','Honeysuckle','Saltalamacchia','Jones','Hollabackatcha','Slicks','Cooper','Milkington','Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles','Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf','Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+'Stevens', 'Stroganoff', 'Sugar­Gold', 'Swackhamer', 'Tippins',
+'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+'Woolysocks')
 
-while true:
+while True:
   first = random.choice(first)
   last = random.choice(last)
 
