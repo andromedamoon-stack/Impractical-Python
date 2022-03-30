84e02fcfbe51fbfaf6a7f64a1684fe43d8d190d7
Sarah W <83520578+andromedamoon-stack@users.noreply.github.com>
2022-03-30 20:59:41 +0000

    defined main function, adjusted tryAgain.

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import random
 print("Welcome to the Psych Sidekick Name Generator")
 print("A name that Shawn would give Gus:'\n'")
 
-
-first = ('SuperSmeller','Francois','Peter Panic','Felicia','Gus','Ovaltine','Shmuel','Magic','Don','Galileo','The','Schoonie','Lavender','Hummingbird','Scrooge','Sterling','Dr.','Step','Trapezious','Jazz','Doughnut','Clemintine','The Vault Of','Ghee','Pennywhistle', 'Pitchfork Ben', 'Potato Bug','Pushmeet','Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+def main():
+  first = ('SuperSmeller','Francois','Peter Panic','Felicia','Gus','Ovaltine','Shmuel','Magic','Don','Galileo','The','Schoonie','Lavender','Hummingbird','Scrooge','Sterling','Dr.','Step','Trapezious','Jazz','Doughnut','Clemintine','The Vault Of','Ghee','Pennywhistle', 'Pitchfork Ben', 'Potato Bug','Pushmeet','Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
 "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
 'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
 'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
 "Winston 'Jazz Hands'", 'Worms')
-last = ('Fancybottom', 'Smeller','Lambert Watkins','T.T Showbiz','Jenkins','Cohen','Head','Humpkins','Jackal','U-turn SIngleton','Gooms','Honeysuckle','Saltalamacchia','Jones','Hollabackatcha','Slicks','Cooper','Milkington','Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles','Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf','Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+  last = ('Fancybottom', 'Smeller','Lambert Watkins','T.T Showbiz','Jenkins','Cohen','Head','Humpkins','Jackal','U-turn SIngleton','Gooms','Honeysuckle','Saltalamacchia','Jones','Hollabackatcha','Slicks','Cooper','Milkington','Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles','Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf','Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
 'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
 'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
 'Stevens', 'Stroganoff', 'Sugar­Gold', 'Swackhamer', 'Tippins',
@@ -18,15 +18,18 @@ last = ('Fancybottom', 'Smeller','Lambert Watkins','T.T Showbiz','Jenkins','Cohe
 'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
 'Woolysocks')
 
-while True:
-  first = random.choice(first)
-  last = random.choice(last)
+  while True:
+    first = random.choice(first)
+    last = random.choice(last)
 
-  print("\n\n")
-  print("{}{}".format(first,last), file=sys.stderr)
-  print("\n\n")
+    print("\n\n")
+    print("{}{}".format(first,last), file=sys.stderr)
+    print("\n\n")
 
-tryAgain = input("\n Try Again? press N to quit\n")
-if tryAgain.lower == 'n':
-  break
-input("Press ENTER to exit\n")
+  tryAgain = input("\n Try Again? press Enter else n to quit\n")
+  if tryAgain.lower == 'n':
+    break
+  input("\n Press Enter to Exit.")
+if __name__=='__main__':
+    main()
+  
